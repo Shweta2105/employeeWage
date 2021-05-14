1fa1d2b515192d6f2e128bdb692ef0c6e07419dd
Shweta2105 <sghadi21@gmail.com>
2021-05-14 16:00:33 +0530

    Added: Employee partime added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,13 +1,19 @@
 #!/bin/bash -x
 
 empRatePerHr=20
-empCheck=$((RANDOM%2))
+empCheck=$((RANDOM%3))
 isPresent=1
-
+isParttime=2
 if [ $empCheck -eq $isPresent ]
 then
 	echo "Employee is present"
-	empHrs=8
+
+		empHrs=8
+elif [ $empCheck -eq $isParttime ]
+then
+		echo "Employee working parttime"
+		empHrs=4
+
 else
 	echo "Employee is absent"
 	empHrs=0
