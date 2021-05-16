5e8ffd5822c83b1d162a440744d007d1ad7beb1c
Shweta2105 <sghadi21@gmail.com>
2021-05-16 11:21:02 +0530

    Added: Function added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -9,24 +9,30 @@ MAX_HRS_IN_MONTH=100
 #variables
 totalEmpHrs=0
 totalWorkingDays=0
+
+function getWorkingHours() {
+ case $1 in
+			$IS_FULLTIME)
+				empHrs=8
+         ;;
+
+         $IS_PARTIME)
+            empHrs=4
+         ;;
+         *)
+            empHrs=0
+         ;;
+ esac
+echo $empHrs
+}
+
 while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_OF_WORKING_DAYS ]]
 do
         ((totalWorkingDays++))
-        empCheck=$((RANDOM%3))
+			empCheck=$((RANDOM%3))
+			workHours="$(getWorkingHours $empCheck )"
 
-        case $empCheck in
-                $IS_FULLTIME)
-                        empHrs=8
-                ;;
-
-                $IS_PARTIME)
-                        empHrs=4
-                ;;
-                *)
-                        empHrs=0
-                ;;
-        esac
-totalEmpHrs=$(($totalEmpHrs+$empHrs))
+			totalEmpHrs=$(($totalEmpHrs+$workHours))
 done
 wage=$(($EMP_RATE_PER_HR*$totalEmpHrs))
 
